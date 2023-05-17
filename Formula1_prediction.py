256f9514dd79b67b0f2420472e3ecd9a2f704aff
SwasthikBangera <swasthik.bangera@gmail.com>
2023-05-17 15:12:55 -0700

    Testing update

---
+++ Formula1_prediction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 09:33:24 2023
+
+@author: SwasthikBangera
+@project: Formula1 winner prediction
+
+"""
+
+# Importing the dependencies
+
+import pandas as pd
+import numpy as np
+
+
+
+constructer_df = pd.read_csv('constructors.csv')
+
+print(constructer_df)
