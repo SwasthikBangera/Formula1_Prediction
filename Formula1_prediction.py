f73ca548a12771556c210b7324fe73e82b8b96c2
Swasthik Bangera <37939646+SwasthikBangera@users.noreply.github.com>
2023-05-25 10:13:11 -0700

    Updated Python file

--- Formula1_prediction.py
+++ Formula1_prediction.py
@@ -12,11 +12,43 @@ Created on Tue May 16 09:33:24 2023
 
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
+# Load the data
+df = pd.read_csv("formula1_data.csv")
 
+# Create the features
+features = [
+    "driver_name",
+    "team_name",
+    "car_model",
+    "circuit_name",
+    "weather",
+    "track_temperature",
+    "air_temperature",
+    "race_distance",
+]
 
-constructor_df = pd.read_csv('constructors.csv')
+# Create the target variable
+target = "winner"
 
-print(constructer_df)
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.25)
 
-driver_df = pd.read_csv('drivers.csv')
+# Create the model
+model = LogisticRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions
+predictions = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
+
+# Print the predicted winner for the next race
+next_race = "Spanish Grand Prix"
+predicted_winner = model.predict_proba(df[features][df["race_name"] == next_race])[0].argmax()
+print("Predicted winner for the {}: {}".format(next_race, df[features]["driver_name"][predicted_winner]))
